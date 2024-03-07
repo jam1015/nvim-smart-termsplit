1dddf97034ba7b997c96181d035d1192163a818b
Jordan Mandel <jordan.mandel@live.com>
2024-03-06 21:22:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Neovim Smart Terminal Tplit
+
+## Features
+
+-   **Automatic Terminal Buffer Closure**: Automatically closes terminal
+    buffers upon exit, keeping the workspace clean. Will become unnecessary in future releases of Neovim.
+-   **Context Preservation**: For Unix/Linux users, terminals opened
+    through splits will automatically navigate to the working directory
+    of the current buffer, ensuring a seamless workflow.
+
+## Installation
+
+To install this plugin using
+[lazy.nvim](https://github.com/kdheepak/lazy.nvim), add the following
+configuration to your Neovim setup:
+
+```lua
+require("lazy").setup({
+    {
+        'jam1015/neovim-smart-terminal',
+        config = function()
+            require('neovim-smart-terminal').setup()
+        end
+    }
+})
+```
+
+## Usage
+
+After installation, the plugin provides two new commands within Neovim
+to manage terminal splits:
+
+-   `:Tsplit` - Opens a new terminal in a horizontal split.
+-   `:Tvsplit` - Opens a new terminal in a vertical split.
+
+Additionally, you can bind keyboard shortcuts to these actions for even
+quicker access:
+
+
+## Configuration
+
+```lua
+local opts = { remap = false, silent = true }
+local keymap = vim.keymap.set
+keymap("n", "<C-a>s", require('neovim-smart-terminal').term_hsplit, opts)
+keymap("t", "<C-a>s", require('neovim-smart-terminal').term_hsplit, opts)
+keymap("n", "<C-a>v", require('neovim-smart-terminal').term_vsplit, opts)
+keymap("t", "<C-a>v", require('neovim-smart-terminal').term_vsplit, opts)
+```
+
+
+## License
+
+This project is licensed under the GNU General Public License v3.0. For
+more information, see the LICENSE file in the repository.
---
+++ lua/nvim_smart_termsplit/init.lua
@@ -0,0 +1,81 @@
+local M = {}
+
+
+local function terminal_close_autocmd()
+	local current_buf = vim.api.nvim_get_current_buf()
+	local augroup_name = tostring(current_buf) ..
+			"_" .. vim.fn.jobpid(vim.b.terminal_job_id) .. "_" .. string.format('%x', os.time() + math.random(1000))
+
+	local function delete_term_buf_autocmd(ev_outer)
+		local function close_term_buffer(ev_inner)
+			local buf = ev_inner.buf
+			vim.cmd([[bdelete ]] .. buf)
+
+			return true
+		end
+		vim.api.nvim_create_autocmd("TermClose",
+			{
+				group = augroup_name,
+				buffer = ev_outer.buf,
+				callback = close_term_buffer,
+				once = true
+			})
+		return true
+	end
+
+	vim.api.nvim_create_augroup(augroup_name, { clear = false })
+
+	vim.api.nvim_create_autocmd("TermOpen",
+		{
+			group = augroup_name,
+			callback = delete_term_buf_autocmd,
+			once = true,
+		})
+end
+
+function M.term_vsplit()
+	if vim.bo.buftype == 'terminal' then
+		local pid = vim.fn.jobpid(vim.b.terminal_job_id)
+		local pwd
+		if vim.fn.has('win32') == 1 then  -- For Windows
+			vim.cmd('vsplit | term')
+		elseif vim.fn.has('unix') == 1 then -- For Unix/Linux
+			pwd = vim.fn.systemlist('readlink /proc/' .. pid .. '/cwd')[1]
+			if pwd == "" or pwd == nil then
+				pwd = vim.fn.systemlist('lsof -p ' .. pid .. ' | grep cwd | awk \'{print $NF}\'')[1]
+			end
+			vim.cmd('vsplit')
+			terminal_close_autocmd()
+			vim.cmd('term sh -c \'cd "' .. pwd .. '" && exec $SHELL\'')
+		end
+	else
+		vim.cmd('vsplit')
+	end
+end
+
+function M.term_hsplit()
+	if vim.bo.buftype == 'terminal' then
+		local pid = vim.fn.jobpid(vim.b.terminal_job_id)
+		local pwd
+		if vim.fn.has('win32') == 1 then  -- For Windows
+			vim.cmd('split | term')
+		elseif vim.fn.has('unix') == 1 then -- For Unix/Linux
+			pwd = vim.fn.systemlist('readlink /proc/' .. pid .. '/cwd')[1]
+			if pwd == "" or pwd == nil then
+				pwd = vim.fn.systemlist('lsof -p ' .. pid .. ' | grep cwd | awk \'{print $NF}\'')[1]
+			end
+			vim.cmd('split')
+			terminal_close_autocmd()
+			vim.cmd('term sh -c \'cd "' .. pwd .. '" && exec $SHELL\'')
+		end
+	else
+		vim.cmd('split')
+	end
+end
+
+function M.setup()
+	vim.api.nvim_create_user_command('Tsplit', M.term_hsplit, { bar = true })
+	vim.api.nvim_create_user_command('Tvsplit', M.term_vsplit, { bar = true })
+end
+
+return M
